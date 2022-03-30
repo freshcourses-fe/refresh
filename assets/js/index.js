0daf27d99eec2fde9d9b9bbeb89643e3c195fae6
Kirill <freshjs.ftd.@gmail.com>
2022-03-30 19:56:33 +0300

    Add map

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,3 +37,42 @@ function printUserAddress({ address: { country, city } }) {
   return `Country: ${country}
 City: ${city}`;
 }
+
+// Map && Set
+
+const map = new Map();
+
+map.set(printUserAddress, 'test 0');
+map.set(1, 'test 1');
+map.set(2, 'test 2');
+map.set(1, 'test 212321');
+map.set('1', 'test next');
+const test2134 = map.get(printUserAddress);
+map.has(printUserAddress);
+const result = map.delete(2);
+const result2 = map.delete(2);
+
+// map.clear();
+
+// for (const [key, value] of map) {
+//   console.log(key);
+//   console.log(value);
+// }
+
+// map.forEach((value, key, currentMap) => {
+//   console.log(key);
+//   console.log(value);
+//   console.log(currentMap);
+// });
+// for (const [key, value] of map) {
+//   console.log(key);
+//   console.log(value);
+// }
+
+// for (const key of map.keys()) {
+//   console.log(key);
+// }
+
+// for (const value of map.values()) {
+//   console.log(value);
+// }

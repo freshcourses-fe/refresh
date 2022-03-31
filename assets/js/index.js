83ee12087bcbb7c6237d1e3ff90d85af37a94107
Kirill <freshjs.ftd.@gmail.com>
2022-03-31 20:47:07 +0300

    Add rest and spread

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,57 @@
-function countdown(number) {
-  debugger;
-  if (number <= 0) {
-    console.log('go');
-  } else {
-    console.log(number);
-    countdown(number - 1);
+// Rest operator
+
+function sum(number = 0, ...numbers) {
+  let result = number;
+
+  for (let i = 0; i < numbers.length; i++) {
+    result += numbers[i];
   }
+
+  return result;
 }
 
-/*
-  дан пустой массив
-  написать функцию которая будет добавлять в него строку
-  которую введет пользователь промптом
-  функция как параметр принимает число раз, которое 
-  она будет запускатся
-*/
+const arr1 = [1, 2, 3, 4, 5];
 
-const arr = [];
+const [a, b, c, ...arr2] = arr1;
 
-function addToArray(number) {
-  if (number > 0) {
-    arr.push(prompt('Enter string'));
-    addToArray(number - 1);
-  }
-  return arr;
-}
+const user = {
+  name: '213',
+  lsatName: '3243423',
+  phone: '3546543541',
+};
+
+const { phone, ...userWithoutPhone } = user;
+
+// spread
+
+const numbers1 = [1, 2, 3];
+const numbers2 = [9, 8, 7];
+const str = 'test';
+
+const numbers = [...numbers1, ...numbers2, ...str];
+
+const defaultSettings = {
+  language: 'en',
+  currency: 'USD',
+};
+
+const userSettings = {
+  language: 'ua',
+  currency: 'BTC',
+};
+
+const finalSettings = {
+  test: '21333',
+  ...defaultSettings,
+  ...userSettings,
+  currency: 'UAH'
+};
+
+const finalSettings2 = {
+  test: '21333',
+  language: 'en',
+  currency: 'USD',
+  language: 'ua',
+  currency: 'BTC',
+  ...arr1
+};

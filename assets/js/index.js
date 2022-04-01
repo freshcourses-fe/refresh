3790369698c3239be41008bafea35a6678eae96f
Kirill <freshjs.ftd.@gmail.com>
2022-04-01 18:28:12 +0300

    Add error handling

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,57 +1,32 @@
-// Rest operator
+try {
+  console.log(1);
 
-function sum(number = 0, ...numbers) {
-  let result = number;
+  throw new Error('error 1');
 
-  for (let i = 0; i < numbers.length; i++) {
-    result += numbers[i];
-  }
-
-  return result;
+  console.log(2);
+} catch (err) {
+  console.log('error 1');
+} finally {
+  console.log('end 1');
 }
 
-const arr1 = [1, 2, 3, 4, 5];
+try {
+  console.log(1);
 
-const [a, b, c, ...arr2] = arr1;
+  throw new Error('error 2');
+} catch (err) {
+  if (err instanceof TypeError) {
+    console.log('type error');
+  } else if (err instanceof Error) {
+    console.log('just error');
+  }
+}
 
-const user = {
-  name: '213',
-  lsatName: '3243423',
-  phone: '3546543541',
-};
+try {
+  console.log(1);
 
-const { phone, ...userWithoutPhone } = user;
-
-// spread
-
-const numbers1 = [1, 2, 3];
-const numbers2 = [9, 8, 7];
-const str = 'test';
-
-const numbers = [...numbers1, ...numbers2, ...str];
-
-const defaultSettings = {
-  language: 'en',
-  currency: 'USD',
-};
-
-const userSettings = {
-  language: 'ua',
-  currency: 'BTC',
-};
-
-const finalSettings = {
-  test: '21333',
-  ...defaultSettings,
-  ...userSettings,
-  currency: 'UAH'
-};
-
-const finalSettings2 = {
-  test: '21333',
-  language: 'en',
-  currency: 'USD',
-  language: 'ua',
-  currency: 'BTC',
-  ...arr1
-};
+  // throw new Error('error 3');
+} finally {
+  console.log('end 2');
+}
+const test = 213;

bf02e64a15ee6b2b3b2d73171c9e2663f7bec3ee
Kirill <freshjs.ftd.@gmail.com>
2022-04-04 20:20:22 +0300

    Add reset button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 const btn = document.getElementById('btn');
+const btn2 = document.getElementById('btn2');
 
 let counter = 0;
 
@@ -10,8 +11,14 @@ const clickHandler = () => {
   // counterSpan.innerHTML = `<span onmouseenter="window.close()">${++counter}</span>`;
 };
 
+const clearScorehandler = () => {
+  counter = 0;
+  counterSpan.innerText = counter;
+};
+
 btn.addEventListener('click', clickHandler);
+btn2.addEventListener('click', clearScorehandler);
 
 /*
-  Приделать кликеру кнопку обнуления счета
+  Приделать кликеру кнопку обнуления счетчика и счета
 */
